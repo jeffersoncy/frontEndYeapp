78cb7eaa36238c296c80ab3cc973f17eb0f4c913
jeffersoncy <48133501+jeffersoncy@users.noreply.github.com>
2022-04-24 06:23:01 -0500

    vista escanner 80%

--- src/app/administrador/escaner/escaner.component.ts
+++ src/app/administrador/escaner/escaner.component.ts
@@ -1,6 +1,7 @@
 import {Component, ViewChild, ViewEncapsulation, OnInit} from '@angular/core';
 import {QrScannerComponent} from 'angular2-qrscanner';
 import { ServiceService } from 'src/app/Service/service.service';
+import { Producto } from 'src/app/Modelo/Producto';
 
 @Component({
   selector: 'app-escaner',
@@ -9,8 +10,11 @@ import { ServiceService } from 'src/app/Service/service.service';
   encapsulation: ViewEncapsulation.None
 })
 export class EscanerComponent implements OnInit {
-  @ViewChild(QrScannerComponent, { static: false}) qrScannerComponent: QrScannerComponent ;
 
+  @ViewChild(QrScannerComponent, { static: false}) qrScannerComponent: QrScannerComponent;
+
+  public producto: Producto = new Producto;
+  public bandera: String = "false"
   constructor(private service:ServiceService) { }
 
   ngOnInit(): void {
@@ -42,11 +46,10 @@ export class EscanerComponent implements OnInit {
   });
 
   this.qrScannerComponent.capturedQr.subscribe(result => {
+      this.bandera = "True"
       console.log(result);
       this.service.getProductByID(Number(result)).subscribe(res=>{
-        console.log(res.nomProducto)
-        console.log(res.estado)
-        console.log(res.tipo)
+          this.producto = res;
       })
   });
   }
